0b1589ebb61d981e680d978a6747c25709b73d7c
Alex Sexton <alex@pierre.co>
2025-10-02 17:26:01 -0500

    make the page slightly nicer

--- apps/docs/app/shadcn/page.tsx
+++ apps/docs/app/shadcn/page.tsx
@@ -1,4 +1,5 @@
 import { GitPlatformSync } from '@/registry/new-york/blocks/git-platform-sync/git-platform-sync';
+import { DynamicCodeBlock } from 'fumadocs-ui/components/dynamic-codeblock';
 
 export default function Home() {
   return (
@@ -12,17 +13,59 @@ export default function Home() {
           distribution of our components using the shadcn tooling.
         </p>
       </header>
+      <aside className="flex flex-col gap-4">
+        <h2 className="text-2xl font-bold tracking-tight">Installation</h2>
+        <h3>Add the pierre registry to your project</h3>
+        <DynamicCodeBlock
+          lang="jsonc"
+          code={`{
+  // Additional config
+  "registries": {
+    "@pierre": "https://pierrejs-docs.vercel.app/r/{name}.json"
+    // Additional registries
+  }
+}`}
+        />
+      </aside>
       <main className="flex flex-col flex-1 gap-8">
+        <h2 className="text-2xl font-bold tracking-tight">
+          Available components
+        </h2>
+        <h2
+          id="git-platform-sync"
+          className="text-lg font-bold font-mono tracking-tight"
+        >
+          GitPlatformSync
+        </h2>
         <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[300px] relative bg-background">
-          <div className="flex items-center justify-between">
-            <h2 className="text-sm text-muted-foreground sm:pl-3">
-              Git Platform Sync
-            </h2>
-            {/* <OpenInV0Button name="git-platform-sync" className="w-fit" /> */}
-          </div>
+          {/* <div className="flex items-center justify-between">
+            <OpenInV0Button name="git-platform-sync" className="w-fit" /> 
+          </div> */}
           <div className="flex flex-col items-center justify-center min-h-[280px] relative gap-4">
             <GitPlatformSync />
           </div>
+        </div>
+
+        <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[300px] relative bg-background">
+          Add the component to your project
+          <DynamicCodeBlock
+            lang="sh"
+            code={`npx shadcn@latest add @pierre/git-platform-sync`}
+          />
+          Import the component from your components alias
+          <DynamicCodeBlock
+            lang="tsx"
+            code={`import { GitPlatformSync } from '@/components/blocks/git-platform-sync';
+
+export function Page() {
+  return (
+    <div>
+      <GitPlatformSync />
+    </div>
+  );
+}
+`}
+          />
         </div>
       </main>
     </div>
